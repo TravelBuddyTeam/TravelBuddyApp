dd189520f2c9df3075e105a71c540a4f2f13fb47
Steven "Grant" Carey <careyst1@msu.edu>
2020-04-27 14:13:24 -0400

    Profile Tab (In Progress 4)

--- Travelbuddy/profileViewController.swift
+++ Travelbuddy/profileViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
-class profileViewController: UIViewController {
+class profileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
 
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var locationsCountLabel: UILabel!
     @IBOutlet weak var friendsCountLabel: UILabel!
     @IBOutlet weak var userNameLabel: UILabel!
@@ -28,7 +30,31 @@ class profileViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onProfileImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
